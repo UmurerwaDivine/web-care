57a2101c2020f2f69294bab85eff0ba1c5785d60
UmurerwaDivine <divinekagame@gmail.com>
2019-09-07 19:53:11 +0200

    second commit

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ $(document).ready(function () {
           });
         });
       });
+      function DisableMonday(date) {
+         
+        var day = date.getDay();
+       // If day == 1 then it is MOnday
+       if (day == 1) {
+       
+       return [false] ; 
+       
+       } else { 
+       
+       return [true] ;
+       }
+        
+      }
+        
+       $(function() {
+       $( "#datepicker" ).datepicker({
+       beforeShowDay: DisableMonday
+       });
+       });
